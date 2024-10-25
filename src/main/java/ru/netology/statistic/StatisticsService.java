182f97d7c39990f2261578450540838df07399e1
Nikolay <masknik1666@yandex.ru>
2024-10-25 23:02:07 +0300

    setup jococo and add extra test

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -2,7 +2,7 @@ package ru.netology.statistic;
 
 public class StatisticsService {
     public long findMax (long[] incomes){
-        long currentMax = incomes[8];
+        long currentMax = incomes[0];
         for (long income : incomes){
             if (currentMax < income){
                 currentMax = income;
